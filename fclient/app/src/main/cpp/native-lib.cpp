ab7dceae394bfb8a74109f447527b652ce6730e8
Yan <_email@example.com>
2026-03-11 00:50:02 +0300

    Initial commit

---
+++ fclient/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,10 @@
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_ru_iu3_fclient_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
